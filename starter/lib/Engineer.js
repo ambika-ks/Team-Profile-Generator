027c7571880e1b77047d2652a5f8f37694501a24
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2023-02-16 15:26:24 +0000

    Added Engineer.js

--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,1 +1,13 @@
+const Employee = require("./Employee");
+
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer extends Employee{
+    constructor(name,id,email,github){
+        super(name,id,email)
+        this.github=github
+    }
+grtRole(){ return "Engineer"}
+getGithub(){ return this.github}
+}
+
+module.exports=Engineer
