4d254926ef6166e6630909fdf842bc3b7beb159d
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2023-02-16 15:18:14 +0000

    Updated README.md

---
+++ starter/lib/Employee.js
@@ -0,0 +1,14 @@
+// TODO: Write code to define and export the Employee class
+class Employee{
+    constructor(name,id,email){
+        this.name=name;
+        this.id=id;
+        this.email=email;
+    }
+getname(){return this,this.name}
+getId(){return this,this.name}
+getEmail(){return this,this.name}
+getRole(){return this,this.name}
+}
+
+module.exports=Employee;
